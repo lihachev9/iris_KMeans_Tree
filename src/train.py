43fa5c95ae7e7d9f9d1a0200b52fdfc970feaa11
lihachev9 <andreyyy.lihachev@gmail.com>
2022-11-18 03:25:19 +0300

    Добавил метрику важностьи признаков

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import csv
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 
@@ -17,6 +18,13 @@ y = LabelEncoder().fit_transform(y)
 
 kmeans = DecisionTreeClassifier().fit(X, y)
 y_pred = kmeans.predict(X)
-
 with open("data/predict.txt", 'w') as f:
     print(*y_pred, sep='\n', end='', file=f)
+
+feature_name = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+feature_importance = kmeans.feature_importances_
+
+with open("data/importances.csv", "w", newline='') as result:
+    writer = csv.writer(result)
+    writer.writerow(["feature_name", "feature_importance"])
+    writer.writerows(zip(feature_name, feature_importance))
